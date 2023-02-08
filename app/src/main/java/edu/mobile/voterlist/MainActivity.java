e79aa98e2f8c2077d1b570ca73524d39ad32b2a6
Hariharanrs_0828 <57134303+hariTH-0828@users.noreply.github.com>
2023-02-08 12:29:00 +0530

    Local changes

--- app/src/main/java/edu/mobile/voterlist/MainActivity.java
+++ app/src/main/java/edu/mobile/voterlist/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -13,20 +14,24 @@ public class MainActivity extends AppCompatActivity {
 
     RadioGroup radioGroup;
     RadioButton genderBtn;
+    Button pushBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         radioGroup = findViewById(R.id.groupradio);
+        pushBtn = findViewById(R.id.pushBtn);
     }
 
     public void onRadioButtonClicked(View view) {
         int selectedId = radioGroup.getCheckedRadioButtonId();
         genderBtn = findViewById(selectedId);
 
-        if(selectedId == -1){
+        /*if(selectedId == -1){
             Toast.makeText(MainActivity.this,"Nothing selected", Toast.LENGTH_SHORT).show();
-        }else Toast.makeText(MainActivity.this, genderBtn.getText(), Toast.LENGTH_SHORT).show();
+        }else Toast.makeText(MainActivity.this, genderBtn.getText(), Toast.LENGTH_SHORT).show();*/
+
+
     }
 }
