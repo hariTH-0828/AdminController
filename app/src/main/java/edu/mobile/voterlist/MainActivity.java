f27ff66f40b2fa5c22663d56d0e453f11ca194a1
Hariharanrs_0828 <57134303+hariTH-0828@users.noreply.github.com>
2023-02-09 21:29:34 +0530

    Local changes

--- app/src/main/java/edu/mobile/voterlist/MainActivity.java
+++ app/src/main/java/edu/mobile/voterlist/MainActivity.java
@@ -3,15 +3,24 @@ package edu.mobile.voterlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    // Initialize database variables
+    String name, fatherName, voterId, phoneNo, state, assembly, wardNo, epicNo;
+
+    // Initialize Firebase objects
+
+
+    EditText editName, editFatherName, editVoterID, editPhone, editState, editAssembly, editWard, editEpic;
+    String getGender;
     RadioGroup radioGroup;
     RadioButton genderBtn;
     Button pushBtn;
@@ -20,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editName = findViewById(R.id.editName);
+        editFatherName = findViewById(R.id.editFatherName);
+        editVoterID = findViewById(R.id.editVoterID);
+        editPhone = findViewById(R.id.editPhone);
+        editState = findViewById(R.id.editState);
+        editAssembly = findViewById(R.id.editAssembly);
+        editWard = findViewById(R.id.editWard);
+        editEpic = findViewById(R.id.editEpic);
         radioGroup = findViewById(R.id.groupradio);
         pushBtn = findViewById(R.id.pushBtn);
     }
@@ -28,10 +46,29 @@ public class MainActivity extends AppCompatActivity {
         int selectedId = radioGroup.getCheckedRadioButtonId();
         genderBtn = findViewById(selectedId);
 
-        /*if(selectedId == -1){
-            Toast.makeText(MainActivity.this,"Nothing selected", Toast.LENGTH_SHORT).show();
-        }else Toast.makeText(MainActivity.this, genderBtn.getText(), Toast.LENGTH_SHORT).show();*/
+        getGender = genderBtn.toString();
+        Log.d("Gender", getGender);
+    }
 
+    public void onPushClick(View view) {
+        name = editName.getText().toString().trim();
+        fatherName = editFatherName.getText().toString().trim();
+        voterId = editVoterID.getText().toString();
+        phoneNo = editPhone.getText().toString();
+        state = editState.getText().toString();
+        assembly = editAssembly.getText().toString();
+        wardNo = editWard.getText().toString();
+        epicNo = editEpic.getText().toString();
+
+        Log.d("Button_status", "Push clicked");
+
+        if(!name.isEmpty() && !fatherName.isEmpty() && !voterId.isEmpty() && !phoneNo.isEmpty() && !getGender.isEmpty()
+                && !state.isEmpty() && !assembly.isEmpty() && !wardNo.isEmpty() && !epicNo.isEmpty()) {
+
+            Log.d("Status", name+" "+fatherName+" "+voterId);
+        }
 
     }
+
+
 }
---
+++ app/src/main/java/edu/mobile/voterlist/Users.java
@@ -0,0 +1,94 @@
+package edu.mobile.voterlist;
+
+public class Users {
+
+    String name, fatherName, gender, voterId, phoneNo, state, assembly, wardNo, epicNo;
+
+    public Users() {
+    }
+
+    public Users(String name, String fatherName, String gender, String voterId, String phoneNo, String state, String assembly, String wardNo, String epicNo){
+        this.name = name;
+        this.fatherName = fatherName;
+        this.gender = gender;
+        this.voterId = voterId;
+        this.phoneNo = phoneNo;
+        this.state = state;
+        this.assembly = assembly;
+        this.wardNo = wardNo;
+        this.epicNo = epicNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getVoterId() {
+        return voterId;
+    }
+
+    public void setVoterId(String voterId) {
+        this.voterId = voterId;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAssembly() {
+        return assembly;
+    }
+
+    public void setAssembly(String assembly) {
+        this.assembly = assembly;
+    }
+
+    public String getWardNo() {
+        return wardNo;
+    }
+
+    public void setWardNo(String wardNo) {
+        this.wardNo = wardNo;
+    }
+
+    public String getEpicNo() {
+        return epicNo;
+    }
+
+    public void setEpicNo(String epicNo) {
+        this.epicNo = epicNo;
+    }
+
+}
