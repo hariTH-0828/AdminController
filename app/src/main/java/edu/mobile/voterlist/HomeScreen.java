d0ade64c47859fccb3f55127d44ea1df632873c3
Hariharan <hariharan08112001@gmail.com>
2023-05-10 17:00:26 +0530

    Home screen view update

--- app/src/main/java/edu/mobile/voterlist/HomeScreen.java
+++ app/src/main/java/edu/mobile/voterlist/HomeScreen.java
@@ -6,31 +6,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class HomeScreen extends AppCompatActivity {
 
+    FrameLayout createVoter, updateVoter, listVoter, createCandidate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
 
-    public void onCreateVoter(View view) {
-        Intent swithView = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(swithView);
-    }
+        createVoter = findViewById(R.id.onCreateVoter);
+        updateVoter = findViewById(R.id.onUpdateVoter);
+        listVoter = findViewById(R.id.onListVoter);
+        createCandidate = findViewById(R.id.onCreateCandidate);
 
-    public void onUpdateVoter(View view) {
-        Intent switchView = new Intent(getApplicationContext(), UpdateActivity.class);
-        startActivity(switchView);
-    }
 
-    public void onListVoter(View view) {
-        Intent switchView = new Intent(getApplicationContext(), ListActivity.class);
-        startActivity(switchView);
+        createVoter.setOnClickListener(v -> {
+            Intent switchView = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(switchView);
+        });
+
+        updateVoter.setOnClickListener(v -> {
+            Intent switchView = new Intent(getApplicationContext(), UpdateActivity.class);
+            startActivity(switchView);
+        });
+
+        listVoter.setOnClickListener(v -> {
+            Intent switchView = new Intent(getApplicationContext(), ListActivity.class);
+            startActivity(switchView);
+        });
+
+        createCandidate.setOnClickListener(v -> {
+            Log.i("onClickButton", "Button click");
+        });
     }
 }
